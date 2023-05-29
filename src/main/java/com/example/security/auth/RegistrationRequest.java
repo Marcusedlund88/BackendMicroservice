62f0dcc133110fca9aa35b3667dcc6fdec029ec7
Marcus H <78554363+MarcusH3@users.noreply.github.com>
2023-05-29 08:49:44 +0200

    WORKING

---
+++ src/main/java/com/example/security/auth/RegistrationRequest.java
@@ -0,0 +1,20 @@
+package com.example.security.auth;
+
+import com.example.security.user.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistrationRequest {
+
+    private String firstname;
+
+    private String username;
+
+    private String password;
+}
