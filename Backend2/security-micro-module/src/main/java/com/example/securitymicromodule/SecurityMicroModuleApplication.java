f2daea49f27574458ac4e09e09473eed0633b663
Marcus Edlund <marcus.edlund88@gmail.com>
2023-05-30 16:30:17 +0200

    Test

---
+++ Backend2/security-micro-module/src/main/java/com/example/securitymicromodule/SecurityMicroModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitymicromodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityMicroModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityMicroModuleApplication.class, args);
+	}
+
+}
---
+++ Backend2/security-micro-module/src/test/java/com/example/securitymicromodule/SecurityMicroModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitymicromodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityMicroModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
